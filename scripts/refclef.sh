b8a6ae54b417354ef8732f1ad404abfa150e1166
1170300521 <hitzhangyabo2017@163.com>
2020-12-07 19:30:50 +0800

    fix eval problem and keep retina

---
+++ scripts/refclef.sh
@@ -0,0 +1,4 @@
+#python code/main_dist.py "refclef" --ds_to_use='refclef' --bs=16 --nw=4 --epochs=20 --lstm_dim=128 --img_dim=256 --lang_to_use="lstm"
+#python code/main_dist.py "refclef" --ds_to_use='refclef' --bs=8 --nw=4 --resume=True --resize_img="[608,608]"
+python code/main_dist.py "refclef" --ds_to_use='refclef' --bs=16 --nw=4 --resume=True --resize_img="[320, 320]" --only_val=True --lstm_dim=128 --img_dim=256 --lang_to_use="lstm"
+#CUDA_VISIBLE_DEVICES=0,1 python code/main_dist.py "refclef" --ds_to_use='refclef' --bs=32 --nw=4 --resume=True --resume_path="/home/wds/Downloads/referit_try.pth" --only_val=True --only_test=True
